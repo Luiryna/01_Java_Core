fe414f8fd1f20bb265c208698d10a4cbd0f512f0
Luiryna <luiryna322@gmail.com>
2020-04-10 21:20:18 +0300

    4'th task almost ready

--- src/main/java/task4/Runner.java
+++ src/main/java/task4/Runner.java
@@ -1,14 +1,34 @@
 package task4;
 
-import task4.reflection.ReflectionPrinter;
+import task4.reflection.ReflectionCreator;
 import task4.wow.Character;
+import task4.wow.CharacterClass;
+import task4.wow.Race;
 
+import java.lang.reflect.InvocationTargetException;
 
 public class Runner {
     public static void main(String[] args) {
-        ReflectionPrinter reflectionHandler = new ReflectionPrinter();
+        ReflectionCreator reflectionHandler = new ReflectionCreator();
         Class<Character> carClass = Character.class;
         reflectionHandler.printAllFields(carClass);
         reflectionHandler.printAllMethods(carClass);
+
+        try {
+            Character character = reflectionHandler.instantiateCharacterObject("name1", Race.HUMAN, CharacterClass.MAGE);
+            System.out.println(character.toString());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 }
---
+++ src/main/java/task4/reflection/ReflectionCreator.java
@@ -0,0 +1,45 @@
+package task4.reflection;
+
+import task4.wow.Character;
+import task4.wow.CharacterClass;
+import task4.wow.Race;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionCreator {
+    public void printAllFields(Class clss) {
+        Field[] fields = clss.getDeclaredFields();
+        System.out.println("<--- Here are fields --->");
+        for (Field field : fields) {
+                field.setAccessible(true);
+                System.out.println(field.getName());
+        }
+    }
+
+    public void printAllMethods(Class clss) {
+        Method[] methods = clss.getDeclaredMethods();
+        System.out.println("<--- Here are methods --->");
+        for (Method method : methods) {
+            method.setAccessible(true);
+            System.out.println(method.getName());
+        }
+    }
+
+    public Object getClassObject(Class clss) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Object obj = null;
+            obj = clss.getDeclaredConstructor().newInstance();
+        return obj;
+    }
+
+    public Character instantiateCharacterObject(String name, Race race, CharacterClass characterClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Character character = null;
+        Class<Character> characterClss = Character.class;
+        Class[] params = {String.class, Race.class, CharacterClass.class};
+        character = characterClss.getConstructor(params).newInstance(name, race, characterClass);
+        return character;
+    }
+    
+
+}
